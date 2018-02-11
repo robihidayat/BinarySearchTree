2a0c6de32586d9f86894289d08c49950a1252220
robi hidayat <robi.hidayat@mail.ugm.ac.id>
2018-02-11 23:52:36 +0700

    first commit

---
+++ src/com/robihidayat/Main.java
@@ -0,0 +1,10 @@
+package com.robihidayat;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello wolrd");
+        // write your code here
+    }
+}
